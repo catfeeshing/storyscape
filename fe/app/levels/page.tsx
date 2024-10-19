e751d2e296b80679c6928959a58f7f6c4508e1f6
airwuu <1kookyw1@gmail.com>
2024-10-19 05:22:03 -0700

    prettier level select

--- fe/app/levels/page.tsx
+++ fe/app/levels/page.tsx
@@ -6,10 +6,10 @@ export default function BlogPage() {
   return (
     <div>
       <h1 className={title()}>levels</h1>
-      <div className="flex flex-row gap-5 mt-10">
-        <LevelCard name="easy"></LevelCard>
-        <LevelCard name="medium"></LevelCard>
-        <LevelCard name="hard"></LevelCard>
+      <div className="flex flex-row gap-5 mt-10 w-full justify-center">
+        <LevelCard name="easy" src="https://pics.craiyon.com/2023-10-25/589e1ea0f8c54cd1b1d3eda60f394fd1.webp"></LevelCard>
+        <LevelCard name="medium" src="https://img.pikbest.com/ai/illus_our/20230424/5f14e94ae9dc62de5f6ceaded95b306f.jpg!sw800"></LevelCard>
+        <LevelCard name="hard" src="https://www.shutterstock.com/image-vector/cave-lava-underground-hell-landscape-600nw-2023554989.jpg"></LevelCard>
       </div>
       
     </div>
--- fe/components/levelCard.tsx
+++ fe/components/levelCard.tsx
@@ -3,9 +3,10 @@ import {Card, CardHeader, CardBody, CardFooter, Image} from "@nextui-org/react";
 
 interface cardProps {
     name: string
+    src: string
 }
 
-const LevelCard: React.FC<cardProps> = ({name}) => {
+const LevelCard: React.FC<cardProps> = ({name, src}) => {
     const link:string = "/" + name
   return (
     <div>
@@ -15,18 +16,18 @@ const LevelCard: React.FC<cardProps> = ({name}) => {
                 radius="lg"
                 isHoverable
                 isPressable
-                className="border-none hover:translate-y-[-5px]"
+                className="border-none hover:translate-y-[-5px] h-[200px] w-[300px]"
                 >
                 <a href={link}>
                     <Image
-                        alt="Woman listing to music"
-                        className="object-cover "
+                        alt={name}
+                        className="object-cover"
                         height={200}
-                        src="https://www.pocketmindfulness.com/wp-content/uploads/2017/03/baby-steps-approach.jpg"
-                        width={200}
+                        src={src}
+                        width={300}
                     />
-                    <CardFooter className=" justify-centerbefore:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                        <p className="text-tiny text-white/80">{name}</p>
+                    <CardFooter className=" justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                        <p className="text-xl font-extrabold text-white/80">{name}</p>
                     </CardFooter>
                 </a>
             </Card>
