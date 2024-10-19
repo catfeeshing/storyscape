9a608ac941b91728ba851c72b17b92180c3b37e2
airwuu <1kookyw1@gmail.com>
2024-10-19 01:14:16 -0700

    routing for levels

--- components/levelCard.tsx
+++ components/levelCard.tsx
@@ -6,27 +6,30 @@ interface cardProps {
 }
 
 const LevelCard: React.FC<cardProps> = ({name}) => {
+    const link:string = "/" + name
   return (
     <div>
-        <Card
-            isFooterBlurred
-            radius="lg"
-            isHoverable
-            isPressable
-            className="border-none hover:translate-y-[-5px]"
-            >
-            <Image
-                alt="Woman listing to music"
-                className="object-cover "
-                height={200}
-                src="https://www.pocketmindfulness.com/wp-content/uploads/2017/03/baby-steps-approach.jpg"
-                width={200}
-            />
-            <CardFooter className=" justify-centerbefore:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                <p className="text-tiny text-white/80">{name}</p>
-            </CardFooter>
-        </Card>
         
+            <Card
+                isFooterBlurred
+                radius="lg"
+                isHoverable
+                isPressable
+                className="border-none hover:translate-y-[-5px]"
+                >
+                <a href={link}>
+                    <Image
+                        alt="Woman listing to music"
+                        className="object-cover "
+                        height={200}
+                        src="https://www.pocketmindfulness.com/wp-content/uploads/2017/03/baby-steps-approach.jpg"
+                        width={200}
+                    />
+                    <CardFooter className=" justify-centerbefore:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                        <p className="text-tiny text-white/80">{name}</p>
+                    </CardFooter>
+                </a>
+            </Card>
     </div>
   )
 }
