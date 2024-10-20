81740d718a273ec649ae2beafd4c8a6dd13684e3
airwuu <1kookyw1@gmail.com>
2024-10-20 04:40:29 -0700

    legs gone

--- fe/app/levels/page.tsx
+++ fe/app/levels/page.tsx
@@ -1,5 +1,6 @@
 import { title } from "@/components/primitives";
 import LevelCard from "@/components/levelCard";
+import {Image} from "@nextui-org/image";
 
 export default function BlogPage() {
   return (
@@ -11,6 +12,15 @@ export default function BlogPage() {
         <LevelCard name="Intense Infernos" description="Become an expressive/emotional reader" link="/hard" src="https://www.shutterstock.com/image-vector/cave-lava-underground-hell-landscape-600nw-2023554989.jpg"></LevelCard>
       </div>
       <div className="block inset-0 bg-gradient-to-r from-violet-200 to-pink-200 z=0"/>
+      <div className=" inset-0 justify-center align-middle items-end" >
+        <Image
+        alt="NextUI hero Image"
+        src="https://media.discordapp.net/attachments/1296711558868762687/1297522562439450675/IMG_8107.png?ex=67163b8d&is=6714ea0d&hm=356f80633ec08abf3005e059c23ca809ce90d37629fa860e4a57e871718eb809&=&format=webp&quality=lossless&width=645&height=928"
+        width={10000}
+        className="origin-bottom-right rotate-[16] translate-x-[-30px] translate-y-[-500px] transform w-[500px]" 
+      />
+      </div>
     </div>
+    
   );
 }
--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -1,12 +1,25 @@
-
-
-
+import {Image} from "@nextui-org/image";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <h1>welcome to storyscape!</h1>
-
-      
+      {/* <h1>welcome to storyscape!</h1> */}
+      {/* <div style={{backgroundImage: "url('htty-40 bg-center fixed inset-0 "></div> */}
+     <div className="fixed inset-0 justify-center align-middle items-end opacity-50" >
+      <Image
+      alt="NextUI hero Image"
+      src="https://images-ext-1.discordapp.net/external/wEQRfhDXRyLu5HGFlLBBK_9tY3qFY7gKZbrTHrKlNeg/https/cdn.vectorstock.com/i/500p/07/91/fantasy-land-horizontal-background-grass-hills-vector-52000791.jpg?format=webp&width=1226&height=730"
+      width={10000}
+      className="" 
+    />
+    </div>
+    <div className=" inset-0 justify-center align-middle items-end" >
+      <Image
+      alt="NextUI hero Image"
+      src="https://media.discordapp.net/attachments/1296711558868762687/1297499568552022017/Untitled_3.PNG?ex=67162623&is=6714d4a3&hm=17dca45ed24c1b96c4b7c2e247704fc5bd6e98b5b98af88b5c3cbd62bd233a75&=&format=webp&quality=lossless&width=645&height=928"
+      width={10000}
+      className="origin-bottom-right rotate-6 translate-x-64 translate-y-10 transform w-[500px]" 
+    />
+    </div>
       
     </section>
   );
