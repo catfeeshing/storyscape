4d588480bc86ae90691139ec3290d50fa8b44e91
Carolyn Cui <33041211+catfeeshing@users.noreply.github.com>
2024-10-19 19:03:32 -0700

    mode and splitting output

--- fe/app/medium/page.tsx
+++ fe/app/medium/page.tsx
@@ -1,11 +1,27 @@
 import { title } from "@/components/primitives";
 import Prompt from '@/components/prompt'
+import { fetchAccessToken } from "hume";
 
-export default function DocsPage() {
+
+export default async function DocsPage() {
+  globalThis.mode = "medium"
+  const accessToken = await fetchAccessToken({
+
+    apiKey: String(process.env.HUME_API_KEY),
+
+    secretKey: String(process.env.HUME_SECRET_KEY),
+
+    
+  });
+  if (!accessToken) {
+
+    throw new Error();
+
+  }
   return (
     <div>
       <h1 className={title()}>Medium</h1>
-      <Prompt proompt="give me two sentences" instruction="You are part of a web app that teaches kids from 1st grade through middle school how to read! Specifically, you are generating two sentences in this age range. When told \\\'Give me two sentences!\\\', you will generate exactly two (2) sentences that a 4th-6th grader might want to learn. Use simple English that an elementary schooler could understand! Sound more upbeat with the definitions! Don't use \\\'really\\\' that often. Don't use markdown. No asterisks."/>
+      <Prompt token={accessToken} proompt="give me two sentences" instruction="You are part of a web app that teaches kids from 1st grade through middle school how to read! Specifically, you are generating two sentences in this age range. When told \\\'Give me two sentences!\\\', you will generate exactly two (2) sentences that a 4th-6th grader might want to learn. Use simple English that an elementary schooler could understand! Sound more upbeat with the definitions! Don't use \\\'really\\\' that often. Don't use markdown. No asterisks."/>
     </div>
   );
 }
